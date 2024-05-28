aa99d9bfb4b5081279569cba763694d67d7df237
EldoranCodes <johnleonardsalinas08@gmail.com>
2024-05-28 08:55:30 +0800

    paired Index for arrays

---
+++ ConsoleProjects/pairedIndex.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class pairedIndex {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    int index;
+    String Email[] ={"nardodo8@gmail.com", "kingkong@gmail.com", "Borologs@gmail.ocm"} ;
+    String Username[] ={"nardo", "DonkeyKong","borlogin"};
+    String Password[] = {"dranoel", "banana", "borlogpassword"};
+
+    System.out.print("Enter an index (1-3):");
+    index = sc.nextInt();
+    index = index - 1;
+     
+    System.out.println("Index: " + (index+1));
+    System.out.println("Email: " + Email[index]);
+    System.out.println("Username: " + Username[index]);
+    System.out.println("Password: " + Password[index]);
+
+  }
+}
