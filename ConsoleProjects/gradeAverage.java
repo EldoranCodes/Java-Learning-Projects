86b28cd62e4b6f660da4b1445acdb3816e23dfd9
EldoranCodes <johnleonardsalinas08@gmail.com>
2024-06-03 13:09:44 +0800

    adding gradesAverage Excercise for if else

---
+++ ConsoleProjects/gradeAverage.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class gradeAverage{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a grade:");
+        float grade1 = sc.nextInt();
+        System.out.print("Enter a grade:");
+        float grade2 =sc.nextInt();
+        System.out.print("Enter a grade:");
+        float grade3 = sc.nextInt();
+        System.out.print("Enter a grade:");
+        float grade4 = sc.nextInt();
+
+        float average = (grade1 + grade2 + grade3+ grade4)/4;
+        System.out.println("Average: "+average);
+        if (average > 100 ) System.out.println("Invalid Grade");
+        else if (average >= 98 && average <= 100)System.out.println("With Highest Honors");
+        else if (average >= 95 && average <= 97.99)System.out.println("With High Honors");
+        else if (average >= 90 && average <= 94.99)System.out.println("With Honors");
+        else if (average >= 75 && average <= 89.99)System.out.println("Passed");
+        else if (average < 75 )System.out.println("Failed");
+
+        
+        
+    }
+}
